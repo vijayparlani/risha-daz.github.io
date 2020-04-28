266e275540429da3acd44dea29fa0a5fe0f42bad
risha-daz <62460444+risha-daz@users.noreply.github.com>
2020-04-28 23:39:48 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Hamburger Menu
 var isOpen= false;
 var nav2=document.querySelector('.nav2');
 console.log(nav2);
@@ -7,9 +8,19 @@ hammenu.addEventListener('click', (e) => {
     isOpen=!isOpen;
     console.log(isOpen);
     if (isOpen==true)
-    {nav2.style.display='block';
-    console.log('block');}
-    else{nav2.style.display='none' ;
-console.log('hidden')}
-    }
-    )
+    {nav2.style.display='flex';}
+    else{nav2.style.display='none' ;}
+    })
+window.addEventListener('resize',function(e){
+    if(window.innerWidth>1024){nav2.style.display='flex';
+}
+else{nav2.style.display='none';}
+})
+
+//createAI arrow
+create_ai=document.querySelector('.createai');
+arrow=document.querySelector('.arrow')
+window.addEventListener('scroll',(e)=>{
+    create_ai.classList.add('vis');
+    arrow.classList.remove('vis');
+})
